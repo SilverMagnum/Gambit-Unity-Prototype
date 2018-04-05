b5c77171b24f8b98b2bfe7a584edc8ed6e5ee1ea
chase cameron <chase cameron>
2018-04-05 00:17:25 -0700

    Set up basic scene, with menus and appropriate game objects.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu: MonoBehaviour {
+	public int hudMenuIndex;
+	
+	public void Update() {
+		if(Input.GetButtonDown("Pause")) {
+			Time.timeScale = 1f;
+			gameObject.GetComponentInParent<LiteMenuManager>().OpenMenu(
+				hudMenuIndex,
+				true
+			);
+		}
+	}
+	
+	public void OnResumePressed() {
+		Time.timeScale = 1f;
+		gameObject.GetComponentInParent<LiteMenuManager>().OpenMenu(
+			hudMenuIndex,
+			true
+		);
+	}
+
+	public void OnQuitPressed()	{
+		Debug.Log("Quit button pressed!! Program closes.");
+		Application.Quit();
+	}
+}
